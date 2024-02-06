aaa62772d579b592a071c6adfb8c88bf705cfae3
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-02-06 10:48:58 -0500

    fixed some functions and organization

--- main_functions.py
+++ main_functions.py
@@ -1,12 +1,13 @@
 import json
 
 
-def save_to_file(data, filename):
-    with open(filename, 'w') as write_file:
-        json.dump(data, write_file, indent=2)
-
-
-def read_from_file(filename):
-    with open(filename, 'r') as read_file:
+def read_from_file(file_name):
+    with open(file_name, 'r') as read_file:
         data = json.load(read_file)
     return data
+
+def save_to_file(data, file_name):
+    with open(file_name, 'w') as write_file:
+        json.dump(data, write_file, indent=2)
+        print("You successfully saved to {}.".format(file_name))
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,27 @@
+import requests
 import streamlit as st
-
+import pandas as pd
+from nltk import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import main_functions
+import plotly.express as px
 
 # Set up your Streamlit page
 st.set_page_config(
-    page_title="Web Dev - Streamlit",
-    menu_items={
-        'Get Help': 'https://docs.streamlit.io/library/api-reference',
-        'Report a bug': "https://docs.streamlit.io/library/api-reference",
-        'About': "# This is Project 1 - Learning Streamlit for COP 4813 - Jose Suarez"
-    }
+        page_title="Project 1",
+        page_icon="📰",
+        layout="centered",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'Get Help': 'https://docs.streamlit.io/library/api-reference',
+            'Report a bug': "https://docs.streamlit.io/library/api-reference",
+            'About': "# This is Project 1 for COP 4813 - Prof. Gregory Reis"
+        }
 )
+
+stop_words = set(stopwords.words("english"))
+
+st.title("Project 1 - News App")
