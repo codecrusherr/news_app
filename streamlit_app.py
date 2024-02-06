51b8dd183773f1ed96a810f7126984f99a6bd21c
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-02-06 10:35:55 -0500

    Initial commit to setup

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+# Set up your Streamlit page
+st.set_page_config(
+    page_title="Web Dev - Streamlit",
+    menu_items={
+        'Get Help': 'https://docs.streamlit.io/library/api-reference',
+        'Report a bug': "https://docs.streamlit.io/library/api-reference",
+        'About': "# This is Project 1 - Learning Streamlit for COP 4813 - Jose Suarez"
+    }
+)
