c89229fa0adbfb5b9161aa2dce5af4abbc1fbb56
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-02-06 10:50:34 -0500

    fixed some functions and organization

--- main_functions.py
+++ main_functions.py
@@ -6,8 +6,8 @@ def read_from_file(file_name):
         data = json.load(read_file)
     return data
 
+
 def save_to_file(data, file_name):
     with open(file_name, 'w') as write_file:
         json.dump(data, write_file, indent=2)
         print("You successfully saved to {}.".format(file_name))
-
