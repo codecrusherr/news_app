46e60d2b4c4a006d6cbfd871ef694fdb4b475ce0
Jose Suarez <jasuarez4261@eagle.fgcu.edu>
2024-02-06 23:21:43 -0500

    fixed implementation issues with part a and b

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,77 +29,66 @@ stop_words = set(stopwords.words("english"))
 
 st.title("Project 1 - News App")
 
+# Load API key directly at the beginning
+api_key = main_functions.read_from_file("JSON_Files/api_key.json")["my_key"]
 
-# Load API key
-def load_api_key():
-    api_data = main_functions.read_from_file("JSON_Files/api_key.json")
-    return api_data["my_key"]
+app_page = st.sidebar.selectbox("Select an API", ["", "Top Stories", "Most Popular Articles"])
 
-
-app_page = st.sidebar.selectbox("Select an API",
-                                ["", "Top Stories", "Most Popular Articles"])
-
-
-# Function to fetch data from the NYT API
-def fetch_nyt_data(section):
-    api_key = load_api_key()  # Ensure the API key is loaded right where it's needed
-    url = f"https://api.nytimes.com/svc/topstories/v2/{section}.json?api-key={api_key}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return " ".join(article['title'] + ' ' + article['abstract'] for article in response.json()['results'])
-    else:
-        st.error("Error fetching data from the NYT API")
-        return ""
-
-
-# Function to generate word cloud
-def generate_word_cloud(text_wordcloud, max_words, background_color, colormap):
-    user_wordcloud = WordCloud(width=800,
-                               height=800,
-                               stopwords=stop_words,
-                               max_words=max_words,
-                               background_color=background_color,
-                               colormap=colormap).generate(text_wordcloud)
-    fig, ax = plt.subplots()
-    plt.imshow(user_wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    st.pyplot(fig)
-
-
-# Part A: Top Stories API
+# Part A: Top Stories API - Fetching and Processing Directly
 if app_page == "Top Stories":
-    st.subheader("The Stories API")
+    st.header("The Stories API")
     st.subheader("I - WordCloud")
 
-    topic_wordcloud = st.selectbox("Select Topic",
-                                   ["", "arts", "automobiles", "books", "business", "fashion", "food", "health",
-                                    "home",
-                                    "insider",
-                                    "magazine",
-                                    "movies", "nyregion", "opinion", "politics", "realestate", "science", "sports",
-                                    "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us",
-                                    "world"])
+    topic_wordcloud = st.selectbox("Select a topic of your interest", ["", "arts", "automobiles", "books", "business",
+                                                                       "fashion", "food", "health", "home", "insider",
+                                                                       "magazine", "movies", "nyregion", "opinion",
+                                                                       "politics", "realestate", "science", "sports",
+                                                                       "sundayreview", "technology", "theater",
+                                                                       "t-magazine", "travel",
+                                                                       "upshot", "us", "world"])
 
     if topic_wordcloud:
-        text_for_wordcloud = fetch_nyt_data(topic_wordcloud)
+        url = f"https://api.nytimes.com/svc/topstories/v2/{topic_wordcloud}.json?api-key={api_key}"
+        response = requests.get(url)
 
-        col1, col2 = st.columns(2)
+        text_for_wordcloud = ""
+        if response.status_code == 200:
+            articles_data = response.json()['results']
+            # This will process each article's title and abstract
+            for article in articles_data:
+                title = article['title']
+                abstract = article.get('abstract', '')
+                # Concatenate title and abstract with a space for separation
+                text_for_wordcloud += title + ' ' + abstract + " "
+        else:
+            st.error("Failed to fetch data from the NYT API.")
 
-        with col1:
-            max_words_wordcloud = st.slider("Max words in WordCloud", 1, 200, 100)
-            colormap_wordcloud = st.selectbox("Colormap",
-                                              ["prism", "viridis", "plasma", "magma", "cividis", "cool", "spring",
-                                               "autumn", "summer", "winter"])
-            background_color_wordcloud = st.color_picker("Background Color", "#000000")
+        # Check if text_for_wordcloud is not empty
+        if text_for_wordcloud:
+            col1, col2 = st.columns(2)
 
-        with col2:
-            generate_word_cloud(text_for_wordcloud, max_words_wordcloud, background_color_wordcloud, colormap_wordcloud)
+            with col1:
+                max_words_wordcloud = st.slider("Choose a maximum number of words to be displayed", 1, 200, 100)
+                colormap_wordcloud = st.selectbox("Choose a colormap",
+                                                  ["prism", "viridis", "plasma", "magma", "cividis",
+                                                   "cool", "spring", "autumn", "summer", "winter"])
+                background_color_wordcloud = st.color_picker("Choose a background color", "#000000")
+
+            with col2:
+                # Generate word cloud directly here
+                user_wordcloud = WordCloud(width=800, height=800, stopwords=stop_words, max_words=max_words_wordcloud,
+                                           background_color=background_color_wordcloud,
+                                           colormap=colormap_wordcloud).generate(text_for_wordcloud)
+                fig, ax = plt.subplots()
+                plt.imshow(user_wordcloud, interpolation='bilinear')
+                plt.axis('off')
+                st.pyplot(fig)
 
         # Frequency Distribution Plot
         st.subheader("II - Frequency Distribution")
         if st.checkbox("Click here to display the frequency distribution plot"):
             if text_for_wordcloud:
-                num_words = st.slider("How many words do you want to see in the plot?", 1, 20, 10)
+                num_words = st.slider("Choose the number of words", 1, 20, 10)
 
                 words = word_tokenize(text_for_wordcloud.lower())
 
@@ -108,7 +97,6 @@ if app_page == "Top Stories":
                     if w.isalpha():
                         no_punkt.append(w)
 
-                # Load list of stopwords
                 stopwordsEnglish = stopwords.words("english")
 
                 filtered_list = []
@@ -127,45 +115,52 @@ if app_page == "Top Stories":
 
 # Part B: Most Popular Articles API
 if app_page == "Most Popular Articles":
-    st.subheader("The Most Popular Articles")
+    st.header("The Most Popular Articles")
     st.subheader("I - Comparing Most Shared, Viewed and Emailed Articles")
-    # User inputs for article type and period
-    article_type = st.selectbox("Select the type of articles",
+
+    article_type = st.selectbox("Select your preferred set of articles",
                                 ["", "emailed", "shared", "viewed"])
-    period = st.selectbox("Select the period",
+    period = st.selectbox("Select the age of your articles in days",
                           ["", "1", "7", "30"])
 
     # Fetch data from the NYT Most Popular Articles API
     if article_type and period:
-        article_api_key = load_api_key()
-        api_url = f"https://api.nytimes.com/svc/mostpopular/v2/{article_type}/{period}.json?api-key={article_api_key}"
+        api_url = f"https://api.nytimes.com/svc/mostpopular/v2/{article_type}/{period}.json?api-key={api_key}"
         response = requests.get(api_url)
 
         if response.status_code == 200:
             articles_data = response.json()['results']
-            text_for_wordcloud = " ".join(
-                article['title'] + ' ' + (article.get('abstract', '') or '') for article in articles_data)
+            text_for_wordcloud = ""
+
+            for article in articles_data:
+                title = article['title']
+                abstract = article.get('abstract', '')
+                text_for_wordcloud += title + ' ' + abstract + " "
 
             # Configuration and generation of word cloud
             col1, col2 = st.columns(2)
             with col1:
-                max_words_wordcloud = st.slider("Max words in WordCloud", 1, 200, 100)
-                colormap_wordcloud = st.selectbox("Colormap",
+                max_words_wordcloud = st.slider("Choose a maximum number of words to be displayed", 1, 200, 100)
+                colormap_wordcloud = st.selectbox("Choose a colormap",
                                                   ["prism", "viridis", "plasma", "magma", "cividis", "cool", "spring",
                                                    "autumn", "summer", "winter"],
                                                   key='mp_colormap')
-                background_color_wordcloud = st.color_picker("Background Color", "#000000")
+                background_color_wordcloud = st.color_picker("Choose a background color", "#000000")
 
             with col2:
-                # Generate and display the word cloud for Most Popular Articles
-                generate_word_cloud(text_for_wordcloud, max_words_wordcloud, background_color_wordcloud,
-                                    colormap_wordcloud)
+                user_wordcloud = WordCloud(width=800, height=800, stopwords=stop_words, max_words=max_words_wordcloud,
+                                           background_color=background_color_wordcloud,
+                                           colormap=colormap_wordcloud).generate(text_for_wordcloud)
+                fig, ax = plt.subplots()
+                plt.imshow(user_wordcloud, interpolation='bilinear')
+                plt.axis('off')
+                st.pyplot(fig)
 
             # Frequency Distribution Plot
             st.subheader("II - Frequency Distribution")
             if st.checkbox("Click here to display the frequency distribution plot"):
                 if text_for_wordcloud:
-                    num_words = st.slider("How many words do you want to see in the plot?", 1, 20, 10)
+                    num_words = st.slider("Choose the number of words", 1, 20, 10)
 
                     words = word_tokenize(text_for_wordcloud.lower())
 
@@ -174,7 +169,6 @@ if app_page == "Most Popular Articles":
                         if w.isalpha():
                             no_punkt.append(w)
 
-                    # Load list of stopwords
                     stopwordsEnglish = stopwords.words("english")
 
                     filtered_list = []
@@ -193,4 +187,3 @@ if app_page == "Most Popular Articles":
 
         else:
             st.error("Error fetching data from the NYT Most Popular Articles API")
-
